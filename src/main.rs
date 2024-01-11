04e6366133fbd4b69a04a70589da302a23b6d659
nokazn <41154684+nokazn@users.noreply.github.com>
2024-01-12 00:42:06 +0900

    feat: add implementation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-// use windows::{core::*, Foundation::Uri, Web::Syndication::SyndicationClient};
+use encoding_rs::*;
 use windows::{
     core::*,
     Foundation::Uri,
     Web::Syndication::{SyndicationClient, SyndicationFeed},
+    Win32::UI::WindowsAndMessaging::*,
 };
 
 fn get_retrieve_feed_async(uri: Uri) -> Result<SyndicationFeed> {
@@ -14,16 +15,28 @@ fn get_retrieve_feed_async(uri: Uri) -> Result<SyndicationFeed> {
     client.RetrieveFeedAsync(&uri)?.get()
 }
 
-// #[tokio::main]
 fn main() -> Result<()> {
     let uri = Uri::CreateUri(h!(
         "https://feeds.feedburner.com/RecentlyRatedAlbums-AlbumOfTheYear"
     ))?;
     let feed = get_retrieve_feed_async(uri)?;
     let items = feed.Items()?;
+    let items = items
+        .into_iter()
+        .filter_map(|item| {
+            item.Title()
+                .and_then(|t| t.Text())
+                .map(|t| format!(". {}", t.to_string_lossy()))
+                .ok()
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+    let mut text = SHIFT_JIS.encode(&items).0.to_vec();
+    // null終端にする
+    text.push(0);
 
-    for item in items {
-        println!("{:?}", item.Title()?.Text()?.to_string_lossy());
+    unsafe {
+        MessageBoxA(None, PCSTR(text.as_ptr()), s!("Feed of AOTY"), MB_OK);
     }
 
     Ok(())
